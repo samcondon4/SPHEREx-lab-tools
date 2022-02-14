3f6def6cde593dadc86fd4538df75859270ffaa1
Sam Condon <scondon@caltech.edu>
2022-02-14 12:24:55 -0800

    Added more advanced indexing to the hdf5 recorder

--- spherexlabtools/recorders/recorder.py
+++ spherexlabtools/recorders/recorder.py
@@ -15,6 +15,7 @@ class HDF5Recorder(QueueThread):
 
     META_GROUP = "Meta"
     PARAMS_GROUP = "Params"
+    GROUP_DELIMITER = "/"
 
     def __init__(self, cfg, **kwargs):
         """ Initialize a recorder.
@@ -23,15 +24,17 @@ class HDF5Recorder(QueueThread):
         self.filename = cfg["filename"]
         self.store = pd.HDFStore(self.filename)
         # if this is a new hdf file, with no Meta group, add the group #
-        if self.META_GROUP not in self.store.keys():
+        if (self.GROUP_DELIMITER + self.META_GROUP) not in self.store.keys():
             meta_series = pd.Series({"Records": 0})
             self.store.put(self.META_GROUP, meta_series)
-            self.records = 0
+            self.record_num = 0
         else:
-            self.records = self.store.get(self.META_GROUP)
+            self.record_num = self.store.get(self.META_GROUP)["Records"]
 
-        self.record_group_ind = None
-        self.record_group_size = None
+        # data index within the record #
+        self.record_ind = 0
+        # number of data entries in a record #
+        self.record_size = 1
 
     def handle(self, record):
         """ HDF5 recorder handle.
@@ -43,34 +46,78 @@ class HDF5Recorder(QueueThread):
             assert ValueError("The group argument is required for the HDF5Recorder!")
         group = kwargs.pop("group")
 
-        # update record number and write this new value to the global meta-data group
+        # get record size #
+        if "group_records" in kwargs:
+            self.record_size = kwargs.pop("group_records")
+
+        # append received data to store with the current record number and index #
+        data = record.data
+        shape = data.shape
+        index = pd.MultiIndex.from_product([[self.record_num], [self.record_ind], np.arange(shape[0])],
+                                           names=["RecordNum", "RecordInd", "Row"])
+        df = pd.DataFrame(data, index=index)
+        df.sort_index(inplace=True)
+        self.store.append(group, df, **kwargs)
+
+        # write parameters #
+        params = record.params
+        index = pd.MultiIndex.from_tuples([(self.record_num, self.record_ind)], names=["RecordNum", "RecordInd"])
+        param_df = pd.DataFrame(params, index=index)
+        self.store.append(self.PARAMS_GROUP, param_df, data_columns=True)
+
+        # update record index #
+        record_complete = False
+        if self.record_ind < (self.record_size - 1):
+            self.record_ind += 1
+        else:
+            self.record_ind = 0
+            record_complete = True
+
+        # update record number #
+        if record_complete:
+            self.record_num += 1
+            self.store.put(self.META_GROUP, pd.Series({"Records": self.record_num}), data_columns=True)
+
+    def handle_old(self, record):
+        """ HDF5 recorder handle.
+        """
+        kwargs = record.handle
+        if "group" not in kwargs:
+            err_msg = "required group argument not provided to the HDF5Recorder!"
+            logger.error(err_msg)
+            assert ValueError("The group argument is required for the HDF5Recorder!")
+        group = kwargs.pop("group")
+
+        # update record number and write this new value to the global meta-data group #
         write_params = False
         if "group_records" in kwargs:
-            self.record_group_size = kwargs.pop("group_records")
-            if self.record_group_ind is None or not self.record_group_ind < self.record_group_size:
+            self.record_size = kwargs.pop("group_records")
+            if not self.record_ind < self.record_size:
                 write_params = True
                 self.records += 1
-                self.record_group_ind = 0
+                self.record_ind = 1
             else:
-                self.record_group_ind += 1
+                self.record_ind += 1
         else:
             write_params = True
             self.records += 1
-        self.store.put(self.META_GROUP, pd.Series({"Records": self.records}))
+        self.store.put(self.META_GROUP, pd.Series({"Records": self.records}), data_columns=True)
 
         # write record parameters if they are provided #
         params = record.params
-        if params is not None and write_params:
+        if write_params:
             index = pd.Index([self.records], name="Record")
             param_df = pd.DataFrame(params, index=index)
-            self.store.append(self.PARAMS_GROUP, param_df)
+            self.store.append(self.PARAMS_GROUP, param_df, data_columns=True)
 
         # get data, create index and write to the store #
         data = record.data
         shape = data.shape
-        index = pd.MultiIndex.from_product([[self.records], np.arange(shape[0])], names=["Record", "Row"])
+        index = pd.MultiIndex.from_product([[self.records], [self.record_group_ind], np.arange(shape[0])],
+                                           names=["Record", "RecordInd", "Row"])
         df = pd.DataFrame(data, index=index)
-        self.store.append(group, df, index=False, **kwargs)
+        #df.sort_index(inplace=True)
+        self.store.append(group, df, **kwargs)
 
 
 def create_recorders(exp_pkg):
