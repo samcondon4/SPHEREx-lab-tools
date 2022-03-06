15c386bc30adfd1bbc3fb872ef82f4f5274512b4
samcondon4 <scondon@caltech.edu>
2022-03-05 21:10:33 -0800

    Added properties to the new monochromator driver for grating, osf, shutter, and units

--- spherexlabtools/instruments/newport/cs260.py
+++ spherexlabtools/instruments/newport/cs260.py
@@ -1,13 +1,35 @@
 """ This module provides the driver for the Oriel/Newport Cs260 Monochromator.
 """
+import numpy as np
 import subprocess as sp
 from pymeasure.instruments import Instrument
+from pymeasure.instruments.validators import strict_discrete_range, strict_discrete_set
 
 
 class CS260:
 
     wavelength = Instrument.control("WAVE?", "GOWAVE %f", """Float property representing
-    the current wavelength setting in um. or nm.""", get_process=lambda w: w.stdout.decode("utf-8"))
+    the current wavelength setting in um. or nm. This property can be set.""")
+
+    grating = Instrument.control("GRAT?", "GRAT %i", """Integer property representing
+    the current grating setting. This property can be set.""",
+                                 validator=strict_discrete_set,
+                                 values=[1, 2, 3])
+
+    osf = Instrument.control("FILTER?", "FILTER %i", """Integer property representing the current
+    order sort filter setting. This property can be set.""",
+                             validator=strict_discrete_range,
+                             values=[1, 7, 1])
+
+    shutter = Instrument.control("SHUTTER?", "SHUTTER %s", """String property representing the current
+    shutter setting. This property can be set.""",
+                                 validator=strict_discrete_set,
+                                 values=["O", "C"])
+
+    units = Instrument.control("UNITS?", "UNITS %s", """String property representing the current units of the
+    wavelength setting. This property can be set with "NM" for nanometers and "UM" for microns.""",
+                               validator=strict_discrete_set,
+                               values=["NM", "UM"])
 
     def __init__(self, resourceName, **kwargs):
         """ Initialize communication with the CS260 monochromator. Note that the USB interface
@@ -40,8 +62,8 @@ class CS260:
         return cp
 
     def values(self, cmd):
-        args = [self.resource_name, 'ask']
+        args = [self.resource_name, 'ask', cmd]
         cp = sp.run(args, capture_output=True, check=True)
-        return cp
+        return cp.stdout.decode("utf-8")
 
 
