ef8a689cf12d0cbc4de137862306a6330e0dd353
samcondon4 <scondon@caltech.edu>
2022-03-10 15:34:06 -0800

    MonoControlClient class completed.

--- spherexlabtools/applications/spectral_cal/hw.py
+++ spherexlabtools/applications/spectral_cal/hw.py
@@ -2,14 +2,21 @@
 """
 import os
 
+HOST = "131.215.200.118"
+PORT = 6550
+
+rec_name = f"TCPIP::{HOST}::{PORT}::SOCKET"
+
 Mono = {
     "instance_name": "Monochromator",
-    "manufacturer": "newport",
-    "resource_name": os.path.join(os.getcwd(), "spherexlabtools", "instruments",
-                                  "newport", "CS260_DLLs", "C++EXE.exe"),
-    "instrument": "CS260",
+    "manufacturer": "spherex",
+    "resource_name": rec_name,
+    "instrument": "MonoControlClient",
     "params": {
         "units": "UM"
+    },
+    "kwargs": {
+        "timeout": 10000
     }
 }
 
