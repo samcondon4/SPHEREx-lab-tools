0344cae8d53022917bdf91542b2f1ff06dfe2a54
samcondon4 <scondon@caltech.edu>
2022-03-29 13:59:05 -0700

    added routines to upload calibration curves to the lakeshore 218

--- spherexlabtools/instruments/lakeshore/lakeshore218.py
+++ spherexlabtools/instruments/lakeshore/lakeshore218.py
@@ -27,7 +27,7 @@ import logging
 from time import sleep, time
 
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import strict_discrete_set
+from pymeasure.instruments.validators import strict_discrete_set, strict_range
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
@@ -79,3 +79,58 @@ class LakeShore218(Instrument):
             **kwargs
         )
 
+    def delete_cal_curve(self, curve):
+        """ Delete a ls218 user calibration curve.
+
+        :param curve: Integer specifying the curve to delete. Values between 21-28 for user inputs 1-8
+                      are accepted.
+        """
+        # check curve delete value.
+        strict_discrete_set(int(curve), list(range(21, 29)))
+        self.write(f"CRVDEL {curve}")
+
+    def set_cal_curve_hdr(self, curve, name, sn, form, limit, cof):
+        """ Configure a calibration curve header.
+
+        :param curve: Specifies which curve to configure (21-28 for inputs 1-8).
+        :param name: Specifies the name of the curve. Limited to 15 characters.
+        :param sn: Serial number of the sensor the curve is associated with. Limited to 10 characters.
+        :param form: Curve data format. 2 = V/K, 3 = Ohm/K, 4 = log Ohm/K
+        :param limit: Specifies curve temperature limit in Kelvin.
+        :param cof: Specifies curve temperature coefficient. 1 = negative, 2 = positive.
+        """
+        # check inputs #
+        strict_discrete_set(int(curve), list(range(21, 29)))
+        if len(name) > 15:
+            name = name[0:15]
+        if len(sn) > 10:
+            sn = name[0:10]
+        strict_discrete_set(form, [2, 3, 4])
+        strict_discrete_set(cof, [1, 2])
+        self.write(f"CRVHDR {curve},{name},{sn},{form},{limit},{cof}")
+
+    def set_cal_curve_pt(self, curve, index, units_val, temp_val):
+        """ Configure a user calibration curve data point.
+
+        :param curve: Specifies which curve to configure (21-28 for inputs 1-8).
+        :param index: Specifies points index within the curve (1-200).
+        :param units_val: Specifies sensor units for this point to 6 digits.
+        :param temp_val: Specifies corresponding temperature in Kelvin for this point to 6 digits.
+        """
+        strict_discrete_set(int(curve), list(range(21, 29)))
+        strict_range(index, [1, 200])
+        units_val = round(units_val, 6)
+        temp_val = round(temp_val, 6)
+        self.write(f"CRVPT {curve},{index},{units_val},{temp_val}")
+
+    def set_cal_curve_pts(self, curve, units_val, temp_val):
+        """ Set multiple user calibration curve data points.
+
+        :param curve: Specifies which curve to configure (21-28 for inputs 1-8).
+        :param units_val: List of sensor unit values for this input.
+        :param temp_val: List of temperature values corresponding to unit values above.
+        """
+        ind = 0
+        for uval in units_val:
+            self.set_cal_curve_pt(curve, ind, uval, temp_val[ind])
+            ind += 1
