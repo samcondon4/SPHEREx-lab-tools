18b7b2d5f3e6a000e8776a21589db8e19452f530
Sam <scondon@caltech.edu>
2022-09-22 17:03:19 -0700

    New basic recorder structure works. New CsvRecorder class implemented.

--- spherexlabtools/record.py
+++ spherexlabtools/record.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import threading
-from spherexlabtools.parameters import ParameterInspect
+from spherexlabtools.parameters import ParameterInspect, Parameter, BooleanParameter, IntegerParameter
 
 import spherexlabtools.log as slt_log
 
@@ -14,6 +14,18 @@ class Record:
     lock = threading.Lock()
     lock_initialized = True
 
+    # update attributes #
+    avg = BooleanParameter("Average Buffer", default=False)
+    buffer_size = IntegerParameter("Buffer Size", default=1)
+    generate_ancillary = BooleanParameter("Generate Ancillary", default=False)
+
+    # save attributes #
+    filepath = Parameter("Save Path", default=os.path.join(os.getcwd(), "Record"))
+    save_type = Parameter("Save Type", default=".pkl")
+
+    # for compatibility with the Parameter types #
+    parameters = {}
+
     def __init__(self, name, viewer=None, recorder=None, **kwargs):
         """ Initialize a record.
 
@@ -22,13 +34,23 @@ class Record:
         :param recorder: String identifying the recorder associated with this record.
         """
         self.name = name
+        self.viewer = viewer
+        self.recorder = recorder
+        self.recorder_write_path = os.path.join(os.getcwd(), self.name)
         self.data = None
         self.proc_params = None
         self.meta = None
         self.procedure_start_time = None
         self.emit_kwargs = None
         self.to_date = False
+
+        # - parameters - #
+        self.parameters = {}
         ParameterInspect.update_parameters(self)
+
+        # - TODO: this is for backwards compatibility and should be deprecated - #
+        self.ancillary = {}
+        self.buffer = []
 
     def update(self, data, proc_params=None, meta=None, proc_start_time=None, **kwargs):
         self.data = data
@@ -36,6 +58,24 @@ class Record:
         self.meta = meta
         self.procedure_start_time = proc_start_time
         self.emit_kwargs = kwargs
+
+        # update the buffer attribute #
+        dbuf_size = self.buffer_size - len(self.buffer)
+        if dbuf_size > 0:
+            self.buffer.append(data)
+        elif dbuf_size < 0:
+            self.buffer = self.buffer[:dbuf_size]
+            self.buffer[-1] = data
+        else:
+            self.buffer[:-1] = self.buffer[1:]
+            self.buffer[-1] = data
+
+        # update the data attribute #
+        if self.avg:
+            self.data = np.mean(self.buffer, axis=0)
+        else:
+            self.data = self.buffer[-1]
+
         self.to_date = True
 
     def save(self, file_arg=None):
